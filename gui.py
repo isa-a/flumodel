ca1192d62bddf3374483cb99a8afd6c20284cefb
isa-a <ia19@ic.ac.uk>
2021-09-08 17:53:03 +0100

    latest

--- gui.py
+++ gui.py
@@ -47,8 +47,7 @@ def mainwindow():
     
     
     def values():
-        
-        
+
         global readN
         global readi0
         global readr0
@@ -83,16 +82,16 @@ def mainwindow():
         t = np.linspace(0, 160, 160)
 
         # The SIR model differential equations.
-        def deriv(y, t, readN, readbeta, readgamma):
+        def deriv(y, t, N, I0, R0):
             S, I, R = y
-            dS = ((-readbeta * S * I) / readN)
-            dI = ((readbeta * S * I) / readN) - (readgamma * I)
-            dR = (readgamma * I)
+            dS = ((-beta * S * I) / N)
+            dI = ((beta * S * I) / N) - (gamma * I)
+            dR = (gamma * I)
             return dS, dI, dR
         
         # Initial conditions are S0, I0, R0
         # Integrate the SIR equations over the time grid, t.
-        solve = odeint(deriv, (S0, readi0, readr0), t, args=(N, readbeta, readgamma))
+        solve = odeint(deriv, (S0, I0, R0), t, args=(N, beta, gamma))
         S, I, R = solve.T
         
         # Plot the data on three separate curves for S(t), I(t) and R(t)
@@ -100,7 +99,7 @@ def mainwindow():
         ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
         ax.plot(t, S/1000, 'blue', alpha=1, lw=2, label='Susceptible')
         ax.plot(t, I/1000, 'r', alpha=1, lw=2, label='Infected')
-        ax.plot(t, R/1000, 'black', alpha=1, lw=2, label='Recovered with immunity')
+        ax.plot(t, R/1000, 'black', alpha=1, lw=2, label='Recovered')
         ax.set_xlabel('Time in days')
         ax.set_ylabel('Number (1000s)')
         #ax.set_ylim(0,1.2)
