26bf40d60fe5d29ecfc02bdc4ff69139bb443dd7
isa-a <ia19@ic.ac.uk>
2022-01-11 18:20:47 +0000

    latest

---
+++ callibration.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 17:45:56 2022
+
+@author: ISA
+"""
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from  scipy.optimize import root
+
+
+def peak_infections_pct(beta, n_days_total = 100):
+
+    # Total population, N.
+    N = 10000
+    # Initial number of infected and recovered individuals, I0 and R0.
+    I0, R0 = 10, 0
+    # Everyone else, S0, is susceptible to infection initially.
+    S0 = N - I0 - R0
+    J0 = I0
+    # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+    gamma = 1/7
+    # A grid of time points (in days)
+    t = np.linspace(0, n_days_total, n_days_total+1)
+
+    # The SIR model differential equations.
+    def deriv(y, t, N, beta, gamma):
+        S, I, R, J = y
+        dS = ((-beta * S * I) / N)
+        dI = ((beta * S * I) / N) - (gamma * I)
+        dR = (gamma * I)
+        dJ = ((beta * S * I) / N)
+        return dS, dI, dR, dJ
+
+    # Initial conditions are S0, I0, R0
+    # Integrate the SIR equations over the time grid, t.
+    solve = odeint(deriv, (S0, I0, R0, J0), t, args=(N, beta, gamma))
+    S, I, R, J = solve.T
+
+    return np.max(I)/N
+
+
+betas = np.linspace(0,1,101,endpoint = True)
+peak_inf = [peak_infections_pct(b) for b in betas]
+plt.plot(betas, peak_inf)
+plt.plot(betas, 0.1*np.ones(len(betas)))
+
+
+
+root(lambda b: peak_infections_pct(b)-0.1, x0 = 0.5).x
+
+
--- model.py
+++ model.py
@@ -21,14 +21,14 @@ import matplotlib.pyplot as plt
 
 
 # Total population, N.
-N = 1000
+N = 10000
 # Initial number of infected and recovered individuals, I0 and R0.
 I0, R0 = 10, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 J0 = I0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 1/3, 1/7
+beta, gamma = 0.24270553, 1/7
 # A grid of time points (in days)
 t = np.linspace(0, 100, 100+1)
 
@@ -47,18 +47,18 @@ solve = odeint(deriv, (S0, I0, R0, J0), t, args=(N, beta, gamma))
 S, I, R, J = solve.T
 
 #J_diff = J[1:] - J[:-1]
-J_diff = np.diff(J)
+#J_diff = np.diff(J)
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t, S/1000, 'b', alpha=1, lw=2, label='Susceptible')
-ax.plot(t, I/1000, 'r', alpha=1, lw=2, label='Infected')
-ax.plot(t, R/1000, 'black', alpha=1, lw=2, label='Recovered')
-ax.plot(t, J/1000, 'green', alpha=1, lw=2, label='Incidence')
+ax.plot(t, S, 'b', alpha=1, lw=2, label='Susceptible')
+ax.plot(t, I, 'r', alpha=1, lw=2, label='Infected')
+ax.plot(t, R, 'black', alpha=1, lw=2, label='Recovered')
+ax.plot(t, J, 'green', alpha=1, lw=2, label='Incidence')
 #ax.plot(t, J, 'red', alpha=1, lw=2, label='Cumulative incidence')
 #ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
 ax.set_xlabel('Time in days')
-ax.set_ylabel('Number (1000s)')
+ax.set_ylabel('Number')
 #ax.set_ylim(0,1.1)
 #ax.yaxis.set_tick_params(length=0)
 #ax.xaxis.set_tick_params(length=0)
@@ -72,3 +72,12 @@ plt.show()
 
 
 
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+ax.plot(t, I, 'r', alpha=1, lw=2, label='Infected')
+ax.set_xlabel('Time in days')
+ax.set_ylabel('Number')
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+plt.show()
