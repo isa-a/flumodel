11e1a8cdd600501564269f5e7b0d8a518e206f74
isa-a <ia19@ic.ac.uk>
2022-01-18 21:57:43 +0000

    latest

--- callibration.py
+++ callibration.py
@@ -9,13 +9,14 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from  scipy.optimize import root
+from scipy.optimize import minimize
 import pandas as pd
 
 
 def peak_infections(beta, days = 100):
 
     # Total population, N.
-    N = 100000
+    N = 1000
     # Initial number of infected and recovered individuals, I0 and R0.
     I0, R0 = 10, 0
     # Everyone else, S0, is susceptible to infection initially.
@@ -48,9 +49,10 @@ peak_inf = [peak_infections(b) for b in betas]
 plt.plot(betas, peak_inf)
 plt.plot(betas, 0.1*np.ones(len(betas)))
 
+#scipy.optimize
 
 
-root(lambda b: peak_infections(b)-0.3049758655, x0 = 0.5).x
+root(lambda b: peak_infections(b)-0.1, x0 = 0.5).x
 
 
 data = pd.read_csv('data.csv')
@@ -59,3 +61,64 @@ y = data['incidence']
 plt.plot(x,y)
 
 
+
+def residual(x):
+    return (peak_infections(x) - 0.1) ** 2
+
+
+res = minimize(residual, 0.5, method="Nelder-Mead")
+print(res)
+###############################################################################
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from  scipy.optimize import root
+import pandas as pd
+
+def peak_infections(beta, days = 100):
+
+ # Total population, N.
+ N = 1000
+ # Initial number of infected and recovered individuals, I0 and R0.
+ I0, R0 = 10, 0
+ # Everyone else, S0, is susceptible to infection initially.
+ S0 = N - I0 - R0
+ J0 = I0
+ # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+ gamma = 1/7
+ # A grid of time points (in days)
+ t = np.linspace(0, days, days + 1)
+
+ # The SIR model differential equations.
+ def deriv(y, t, N, beta, gamma):
+   S, I, R, J = y
+   dS = ((-beta * S * I) / N)
+   dI = ((beta * S * I) / N) - (gamma * I)
+   dR = (gamma * I)
+   dJ = ((beta * S * I) / N)
+   return dS, dI, dR, dJ
+
+# Initial conditions are S0, I0, R0
+# Integrate the SIR equations over the time grid, t.
+ solve = odeint(deriv, (S0, I0, R0, J0), t, args=(N, beta, gamma))
+ S, I, R, J = solve.T
+
+ return np.max(I)/N
+
+root(lambda b: peak_infections(b)-0.1, x0 = 0.5).x
+
+
+from scipy.optimize import minimize
+
+def residual(x):
+    return (peak_infections(x) - 0.1) ** 2
+
+x0 = 0.5
+res = minimize(residual, x0, method="Nelder-Mead", options={'fatol':1e-04})
+print(res)
+
+
+d = {'Week': [1, 2,3,4,5,6,7,8,9,10,11], 'incidence': [206.1705794,2813.420201,11827.9453,30497.58655,10757.66954,7071.878779,3046.752723,1314.222882,765.9763902,201.3800578,109.8982006]}
+df = pd.DataFrame(data=d)
+df
