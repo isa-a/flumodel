115e2396f9410c599f88d6ab432184996c774ac9
isa-a <ia19@ic.ac.uk>
2022-02-23 21:48:49 +0000

    latest

--- callibration.py
+++ callibration.py
@@ -27,7 +27,7 @@ df = pd.DataFrame(data=d)
 #d = {'Week': t, 'incidence': [0,206.1705794,2813.420201,11827.9453,30497.58655,10757.66954,7071.878779,3046.752723,1314.222882,765.9763902,201.3800578,109.8982006]}
 #df = pd.DataFrame(data=d)
 
-def peak_infections(beta, df):
+def peak_infections(x, df):
  
     # Weeks for which the ODE system will be solved
     #weeks = df.Week.to_numpy()
@@ -35,7 +35,10 @@ def peak_infections(beta, df):
     # Total population, N.
     N = 100000
     # Initial number of infected and recovered individuals, I0 and R0.
-    I0, R0 = 10, 0
+    beta = x[0]
+    I0 = x[1]
+    R0 = 0
+    #I0, R0 = 10, 0
     # Everyone else, S0, is susceptible to infection initially.
     S0 = N - I0 - R0
     J0 = I0
